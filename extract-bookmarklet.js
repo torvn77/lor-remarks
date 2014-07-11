b3bdff4333ba0d2f4b64dc28a47c39ab21a5be04
Tenno Seremel <mystra_x64@fastmail.fm>
2014-07-11 20:42:09 +0400

    2014-07-11

---
+++ extract-bookmarklet.js
@@ -0,0 +1,1 @@
+javascript:(function(){var result = {};var els = document.querySelectorAll('.message-table > tbody > tr > td:first-child');for (var i = 0, len = els.length; i < len; ++i) {var cell = els[i];var name = cell.querySelector('a');if (name) {name = name.textContent;result[name] = cell.nextElementSibling.textContent.trim();}}console.log(JSON.stringify(result));})();
